1b2228cdc04bec966087296380e9208142540f11
10thSSAFY <ssafyiwan@gmail.com>
2024-12-21 15:59:30 +0900

    refactor: useState 함수 위치 이동
    - AppRouter 컴포넌트를 App 컴포넌트에서 관리
    - footer 엘리먼트 추가

--- learning_streak/src/components/App.js
+++ learning_streak/src/components/App.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import AppRouter from "./Router";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Learning Streak</footer>
+    </>
   );
 }
 
