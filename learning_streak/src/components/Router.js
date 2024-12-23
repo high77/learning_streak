0753a28d7678ed704ddfbc8007651405f71ac220
10thSSAFY <ssafyiwan@gmail.com>
2024-12-23 20:25:00 +0900

    feat: 로그아웃 처리 기능
    - 내비게이션 컴포넌트 만들고 라우터 추가
    - 내비게이션에 링크 추가
    - useNavigate로 로그아웃 후 주소 이동

--- learning_streak/src/components/Router.js
+++ learning_streak/src/components/Router.js
@@ -2,13 +2,19 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Signup from "routes/Signup";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route exact path="/" element={<Home />} />
+          <>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/profile" element={<Profile />} />
+          </>
         ) : (
           <>
             <Route exact path="/" element={<Auth />} />
