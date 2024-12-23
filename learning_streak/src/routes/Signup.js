031f4028f5679243079d9319f96ca66b94b4672e
10thSSAFY <ssafyiwan@gmail.com>
2024-12-23 17:25:50 +0900

    feat: 파이어베이스 회원가입 처리
    - 회원가입 폼 구현
    - 파이어베이스 인증기 signInWithEmailAndPassword 사용
    - 로그인과 회원가입 간 react-router-dom의 링크 추가

--- learning_streak/src/routes/Signup.js
+++ learning_streak/src/routes/Signup.js
@@ -1,3 +1,62 @@
-const Signup = () => <span>Signup</span>;
+import { authService } from "fbase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      data = await createUserWithEmailAndPassword(authService, email, password);
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Create Accout" />
+        {error}
+      </form>
+      <span>
+        <Link to="/">Log In</Link>
+      </span>
+    </div>
+  );
+};
 
 export default Signup;
