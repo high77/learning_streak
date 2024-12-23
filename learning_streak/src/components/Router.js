46b0b3d867b1a40208f2e1849fe121832e8c50aa
10thSSAFY <ssafyiwan@gmail.com>
2024-12-23 17:25:50 +0900

    feat: 파이어베이스 회원가입 처리
    - 회원가입 폼 구현
    - 파이어베이스 인증기 signInWithEmailAndPassword 사용
    - 로그인과 회원가입 간 react-router-dom의 링크 추가

--- learning_streak/src/components/Router.js
+++ learning_streak/src/components/Router.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Signup from "routes/Signup";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
@@ -9,7 +10,10 @@ const AppRouter = ({ isLoggedIn }) => {
         {isLoggedIn ? (
           <Route exact path="/" element={<Home />} />
         ) : (
-          <Route exact path="/" element={<Auth />} />
+          <>
+            <Route exact path="/" element={<Auth />} />
+            <Route exact path="/signup" element={<Signup />}/>
+          </>
         )}
       </Routes>
     </Router>
